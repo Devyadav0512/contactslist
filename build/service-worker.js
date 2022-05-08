f99063406cf83cab85773faf18425fc529806e40
Dev Yadav <devyadav5582@gmail.com>
2022-05-09 00:20:41 +0530

    Update service-worker.js

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/phone-directory/precache-manifest.c435af36940a93883d455c38ff2ed8b1.js"
+  "phone-directory/precache-manifest.c435af36940a93883d455c38ff2ed8b1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/phone-directory/index.html", {
+workbox.routing.registerNavigationRoute("phone-directory/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
